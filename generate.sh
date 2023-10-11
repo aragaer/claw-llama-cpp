a0d7fc9a53c84570435f5b13e1a09ec3d29b6e77
aragaer <aragaer@gmail.com>
2023-10-11 16:52:42 +0300

    Replace generate.lisp with generate.sh

--- generate.sh
+++ generate.sh
@@ -1,11 +1,10 @@
-;;; -*- mode:common-lisp -*-
-
-#-quicklisp
+#!/bin/sh
+exec ~/Projects/ccl/lx86cl64 -Q -n -b <<EOF
 (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname))))
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
 
-(ql:quickload :cffi)
+(ql:quickload :cffi :silent t)
 (cffi:load-foreign-library (merge-pathnames
                             "Projects/libresect/build/resect/libresect.so"
                             (user-homedir-pathname)))
@@ -15,3 +14,4 @@
 
 (asdf:load-system :claw-llama-cpp/wrapper)
 (claw:generate-wrapper :claw-llama-cpp)
+EOF
