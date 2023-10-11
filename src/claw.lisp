95ba3d6774566e31f9ce1776c38260338b4f1a99
aragaer <aragaer@gmail.com>
2023-10-11 15:49:12 +0300

    Actual bindings

--- src/claw.lisp
+++ src/claw.lisp
@@ -1,19 +1,13 @@
-(cl:defpackage :llama-cpp
-  (:use :cl)
-  (:export))
-
-(cl:in-package :llama-cpp)
+(uiop:define-package :llama-cpp
+  (:use :cl))
 
 (claw:defwrapper (:claw-llama-cpp
                   (:system :claw-llama-cpp/wrapper)
                   (:headers "llama.h")
                   (:includes :llama-cpp-includes)
                   (:targets ((:and :x86-64 :linux) "x86_64-pc-linux-gnu"))
-                  (:persistent t
-                   :bindings-path "../bindings/"
-                   :depends-on (:claw-utils))
-                  (:language :c++)
-                  (:include-definitions "llama_backend_init"))
+                  (:persistent t :depends-on (:claw-utils))
+                  (:include-definitions "^(llama|LLAMA)_\\w+"))
   :in-package :%llama-cpp
   :trim-enum-prefix t
   :recognize-bitfields t
